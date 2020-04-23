f6d4345e8ec80caeb1b4ea24948b80dd0911a2ad
Austin Evans <unity7591@gmail.com>
2020-04-23 21:59:04 +0000

    Automatically backed up by Learn

---
+++ casefamily.rb
@@ -0,0 +1,37 @@
+case "Robbie"
+
+
+
+when "Austin"
+  puts "I love Renee!!"
+when "Renee"
+  puts "I love my beautiful family and Austin too"
+when "Jeanie"
+  puts "I am a beautiful princess!"
+when "Robbie"
+  puts "Robbie smash! grrrrr!"
+when "Jerry"
+  puts "woof woof"
+when "Woody"
+  puts "woof woof"
+when "Dom"
+  puts "I have the best kids!"
+when "Bobby"
+  puts "Honey, kids, daddy's home!!"
+else
+  puts "New phone who dis?"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
